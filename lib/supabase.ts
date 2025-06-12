8b93f3fc4dfb6e58a10e5165a0a768a41eca687c
FAVE <74864796+Fearless2242@users.noreply.github.com>
2025-06-12 10:40:23 +0100

    Fix

--- lib/supabase.ts
+++ lib/supabase.ts
@@ -1,9 +1,9 @@
 // lib/supabase.ts
 import { createClient } from '@supabase/supabase-js';
 
-const url   = process.env.SUPABASE_URL!;
-const anon  = process.env.SUPABASE_ANON_KEY!;
-const admin = process.env.SUPABASE_SERVICE_ROLE_KEY!;
+const url   = process.env.NEXT_PUBLIC_SUPABASE_URL!;
+const anon  = process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY!;
+const admin = process.env.NEXT_PUBLIC_SUPABASE_SERVICE_ROLE_KEY!;
 
 // client for frontend or public queries
 export const supabase = createClient(url, anon);
